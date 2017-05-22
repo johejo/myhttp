cfcd2dd389d2b57d9313583f99bc9fb0f4e8f027
heijo <mitsuo_h@outlook.com>
2017-05-22 16:24:24 +0900

    commit

--- http1.1.py
+++ http1.1.py
@@ -3,6 +3,7 @@ import socket
 import sys
 import traceback
 
+
 def main():
 
     argv = sys.argv
@@ -14,7 +15,7 @@ def main():
 
     # check commandline parameter
     if (argc != 2):
-        sys.stderr.write('Usage: python http1.0.py [ADDRESS]\n')
+        sys.stderr.write('Usage: python http1.1.py [ADDRESS]\n')
         exit()
 
     try:
@@ -24,6 +25,7 @@ def main():
         index = temp.find(slash)
         domain = temp[:index]
         file_path = temp[index:]
+
         #connect
         address = (socket.gethostbyname(domain), port)
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -37,32 +39,60 @@ def main():
         index = file_path.rfind(slash)
         file_name = file_path[index+len(slash):]
         fout = open(file_name, 'wb')
-        sock_file = sock.makefile()
+        # sock_file = sock.makefile()
         state = 0
+        read = 0
+        length = 1
+        data = ''
+        i = 0
+        stop =0
+        byte = 0
 
-        #read lines
-        for line in sock_file.readlines():
+        while(1):
+            line = ''
+            while(1) :
+                data = sock.recv(1)
+                if (data == '\r') :
+                    read = 1
+                line = line + data
+                # print (line)
+                if (read == 1 and data == '\n'):
+                    break
+
+                if (state ==2 ):
+                    byte += 1
+
+                if (byte >= length) :
+                    break
 
             if (line == 'HTTP/1.1 200 OK\r\n'):
                 state = 1
             if (state == 1 and line == '\r\n'):
                 state = 2
                 continue
+
             if (state == 2):
                 fout.write(line)
+                i+=len(line)
+
+            if (i >= length):
+                break
+
             elif(state == 0):
                 sys.stderr.write('Cannot receive \'HTTP/1.1 200 OK\'\n')
                 exit()
 
-            print line
+            if ('Content-Length: ' in line):
+                index = line.rfind('Content-Length: ');
+                tmp = line[index+len('Content-Length: '):]
+                length = int(tmp)
 
         sock.close()
+        fout.close()
 
     except:
         sys.stderr.write(traceback.format_exc())
         exit()
-
-
 
 if __name__ == '__main__':
     main()
